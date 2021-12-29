410fd656cb25b620d62c2558fd32c08b2e915702
xuejiehe <xuejiehe@kurogame.com>
2021-12-29 17:32:09 +0800

    优化图片资源导入

--- src/assets/js/image.ts
+++ src/assets/js/image.ts
@@ -1,63 +1,36 @@
-import image1 from '~/assets/img/common/001.jpg'
-import image2 from '~/assets/img/common/002.jpg'
-import image3 from '~/assets/img/common/003.jpg'
-import image4 from '~/assets/img/common/004.jpg'
-import image5 from '~/assets/img/common/005.jpg'
-import image6 from '~/assets/img/common/006.jpg'
-import image7 from '~/assets/img/common/007.jpg'
-import image8 from '~/assets/img/common/008.jpg'
-import image9 from '~/assets/img/common/009.jpg'
-import image10 from '~/assets/img/common/010.jpg'
-import image11 from '~/assets/img/common/011.jpg'
-import image12 from '~/assets/img/common/012.jpg'
-import image13 from '~/assets/img/common/013.jpg'
-import image14 from '~/assets/img/common/014.jpg'
-import image15 from '~/assets/img/common/015.jpg'
-import image16 from '~/assets/img/common/016.jpg'
-import image17 from '~/assets/img/common/017.jpg'
-import image18 from '~/assets/img/common/018.jpg'
-import image19 from '~/assets/img/common/019.jpg'
-import image20 from '~/assets/img/common/020.jpg'
-import image21 from '~/assets/img/common/021.jpg'
-import image22 from '~/assets/img/common/022.jpg'
-import image23 from '~/assets/img/common/023.jpg'
-import image24 from '~/assets/img/common/024.jpg'
-import image25 from '~/assets/img/common/025.jpg'
-import image26 from '~/assets/img/common/026.jpg'
-import image27 from '~/assets/img/common/027.jpg'
-import image28 from '~/assets/img/common/028.jpg'
-import image29 from '~/assets/img/common/029.jpg'
-import image30 from '~/assets/img/common/030.jpg'
+interface FileType {
+    [key: string]: any
+}
 
-export default [
-    image1,
-    image2,
-    image3,
-    image4,
-    image5,
-    image6,
-    image7,
-    image8,
-    image9,
-    image10,
-    image11,
-    image12,
-    image13,
-    image14,
-    image15,
-    image16,
-    image17,
-    image18,
-    image19,
-    image20,
-    image21,
-    image22,
-    image23,
-    image24,
-    image25,
-    image26,
-    image27,
-    image28,
-    image29,
-    image30,
-]
+// 导入目录下所有 .jpg 文件
+const files: Record<string, FileType> = import.meta.globEager('/src/assets/img/common/*.jpg')
+const images: string[] = []
+
+Object.keys(files).forEach((path: string) => {
+    const image = files[path]?.default
+    images.push(image)
+})
+
+export default images
+
+/**
+ *  方法二，需要传入文件名称 & 循环次数，不可行
+ */
+
+/**
+ * vite官方文档的解析静态资源URL
+ */
+// function getCommonImageUrl(name: string) {
+//     return new URL(`../img/common/${name}.jpg`, import.meta.url).href
+// }
+
+/**
+ * 返回长度为n的字符串, num长度不够时前面补0
+ */
+// function prefixInteger(num: number, n: number) {
+//     return (Array(n).join('0') + num).slice(-n)
+// }
+
+// for (let i = 1; i <= 30; i++) {
+//     images.push(getCommonImageUrl(prefixInteger(i, 3)))
+// }
